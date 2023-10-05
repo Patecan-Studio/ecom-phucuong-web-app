38e570c5e577189e7a40af055fcde981c03ab3cd
Phi Nguyen <quangphi1231@gmail.com>
2023-10-05 22:20:34 +0700

    add common style file to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Tabbar } from '@/components/common'
 import styles from './page.module.css'
+import '../styles/reset.css'
+import '../styles/common.scss'
 
 export default function Home() {
   return (
