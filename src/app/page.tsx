7be71b89aa8c4bd69de4b0d99f26ec32e269f6c3
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2023-10-06 22:36:18 +0700

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,17 @@
-import { Tabbar } from '@/components/common'
-import styles from './page.module.css'
-import '../styles/common.scss'
-import '../styles/colors.scss'
-import '../styles/fonts.scss'
-import '../styles/reset.css'
+import { Tabbar } from "@/components/common";
+import styles from "./page.module.css";
+import "../styles/common.scss";
+import "../styles/colors.scss";
+import "../styles/fonts.scss";
+import "../styles/reset.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <Tabbar />
-     <div style={{height: '2000px', backgroundColor:'black', color: 'white'}}>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-      <h1>Page</h1>
-     </div>
+      <Tabbar />
+      <div
+        style={{ height: "2000px", backgroundColor: "black", color: "white" }}
+      ></div>
     </main>
-  )
+  );
 }
