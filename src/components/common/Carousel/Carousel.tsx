43d063b79dbdd18613440a36762d00d2e8213af9
Phi Nguyen <quangphi1231@gmail.com>
2023-10-07 00:27:35 +0700

    add library

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from 'embla-carousel-autoplay'
+import "./style.scss";
 
 const Carousel = () => {
-  return (
-    <div>Carousel</div>
-  )
-}
+  const [carouselRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
-export default Carousel
+  return (
+    <div className="carousel" ref={carouselRef}>
+      <div className="carousel__container">
+        <div className="carousel__slide">Slide 1</div>
+        <div className="carousel__slide">Slide 2</div>
+        <div className="carousel__slide">Slide 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
