da642d0e8f4467168ce17651c5d2b21cfd1f6e56
ext-phi.nguyen <ext-phi.nguyen@spdigital.sg>
2023-10-06 12:27:01 +0700

    optimize the code

---
+++ src/components/common/Tabbar/TabbarMenuItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { TabbarMenuItemProps } from "./types";
+
+const TabbarMenuItem = ({ className, label, count, icon }: TabbarMenuItemProps) => {
+  return (
+    <div className={`tabbar__menu__item ${className}`} key={label}>
+      {count ? (
+        <>
+          <div className="tabbar__menu__icon">
+            {icon}
+            <p className="tabbar__menu__count">{count}</p>
+          </div>
+          <div className="tabbar__menu__text">{label}</div>
+        </>
+      ) : (
+        <>
+          <div className="tabbar__menu__icon">{icon}</div>
+          <div className="tabbar__menu__text">{label}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TabbarMenuItem;
