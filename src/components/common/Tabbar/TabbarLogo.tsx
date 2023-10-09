ab3f5f4fd6f47a933beb0955b0f29a42f578cfe4
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2023-10-09 21:48:26 +0700

    change favicon

--- src/components/common/Tabbar/TabbarLogo.tsx
+++ src/components/common/Tabbar/TabbarLogo.tsx
@@ -3,8 +3,9 @@ import Logo from "../Icons/Logo";
 
 const TabbarLogo = () => {
   return (
-    <a className="tabbar__logo" href="#" >
-      <Logo />
+    <a className="tabbar__logo" href="#">
+      <img src="https://nuwwaqzrwtilsxbajubq.supabase.co/storage/v1/object/public/images/static/logo_phucuong%20350px.png" />
+      <span>Phú Cường Group</span>
     </a>
   );
 };
