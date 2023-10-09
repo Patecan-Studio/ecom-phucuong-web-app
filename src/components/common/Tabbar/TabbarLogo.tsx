533ad829cf14e3dee8fa3dc2a6b5fe2412aca7f3
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2023-10-09 22:18:26 +0700

    remove redundant code

--- src/components/common/Tabbar/TabbarLogo.tsx
+++ src/components/common/Tabbar/TabbarLogo.tsx
@@ -5,7 +5,6 @@ const TabbarLogo = () => {
   return (
     <a className="tabbar__logo" href="#">
       <img src="https://nuwwaqzrwtilsxbajubq.supabase.co/storage/v1/object/public/images/static/logo_phucuong%20350px.png" />
-      <span>Phú Cường Group</span>
     </a>
   );
 };
