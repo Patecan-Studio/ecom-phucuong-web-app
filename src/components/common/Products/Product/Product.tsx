39c2c8ac941befd14c902a3366c63627f795d448
ext-phi.nguyen <ext-phi.nguyen@spdigital.sg>
2023-10-11 15:11:01 +0700

    add some more style for image to fit with the info component

--- src/components/common/Products/Product/Product.tsx
+++ src/components/common/Products/Product/Product.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.scss";
 import ProductCardImage from "./ProductImage";
+import ProductInfo from "./ProductInfo";
 
 const ProductCard = () => {
   return (
     <div className="product">
       <ProductCardImage />
+      <ProductInfo />
     </div>
   );
 };
--- src/components/common/Products/Product/ProductImage.tsx
+++ src/components/common/Products/Product/ProductImage.tsx
@@ -4,15 +4,17 @@ import ProductStatus from "./ProductStatus";
 
 const ProductCardImage = () => {
   return (
-    <div className="product__image">
-      <NextImage
-        src="/images/product-images/product-1.jpg"
-        alt="Picture of the author"
-        width={0}
-        height={0}
-        sizes="100vw"
-      />
-      <ProductStatus />
+    <div className="product__image-container">
+      <a href="#" className="product__image">
+        <NextImage
+          src="/images/product-images/product-1.jpg"
+          alt="Picture of the author"
+          width={0}
+          height={0}
+          sizes="100vw"
+        />
+        <ProductStatus />
+      </a>
     </div>
   );
 };
