9db93f2dedfa957602e5e53e4bb6eb8a3a7c6b1a
ext-phi.nguyen <ext-phi.nguyen@spdigital.sg>
2023-10-11 16:06:11 +0700

    add product info

--- src/components/common/Products/Product/ProductInfo.tsx
+++ src/components/common/Products/Product/ProductInfo.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const ProductInfo = () => {
-  return (
-    <div className="product__info">ProductInfo</div>
-  )
-}
+  const discount_price = (15000000).toLocaleString("vn", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  const price = (20000000).toLocaleString("vn", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
 
-export default ProductInfo
+  const priceClassName = discount_price
+    ? "product__price--old"
+    : "product__price";
+
+  return (
+    <div className="product__info">
+      <div className="product__name">Ghế ăn elio - Tribú</div>
+      {discount_price && <h4 className="product__price">{discount_price} đ</h4>}
+      <h4 className={priceClassName}>{price} đ</h4>
+    </div>
+  );
+};
+
+export default ProductInfo;
