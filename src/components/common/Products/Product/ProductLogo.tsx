bea42dd6955e075213904d194fe4bee509b531f8
Phi Nguyen <quangphi1231@gmail.com>
2023-10-11 23:15:07 +0700

    remove warning logs

--- src/components/common/Products/Product/ProductLogo.tsx
+++ src/components/common/Products/Product/ProductLogo.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import NextImage from 'next/image'
+import React from "react";
+import NextImage from "next/image";
 
 const ProductLogo = () => {
   return (
     <a className="product__logo" href="#">
-      <NextImage 
+      <NextImage
         src="/images/product-images/caesar-logo.png"
         alt="Picture of the author"
-        layout="fill"
-        objectFit='contain'
+        width={0}
+        height={0}
+        sizes="100vw"
       />
     </a>
-  )
-}
+  );
+};
 
-export default ProductLogo
+export default ProductLogo;
