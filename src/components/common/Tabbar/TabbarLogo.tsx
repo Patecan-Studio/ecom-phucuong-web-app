cf4b7cad6100d1332004a9a47339abac393a2435
Phi Nguyen <quangphi1231@gmail.com>
2023-10-13 00:34:53 +0700

    finish update logo

--- src/components/common/Tabbar/TabbarLogo.tsx
+++ src/components/common/Tabbar/TabbarLogo.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import Logo from "../Icons/Logo";
+import NextImage from "next/image";
 
 const TabbarLogo = () => {
   return (
     <a className="tabbar__logo" href="#">
-      <img src="https://nuwwaqzrwtilsxbajubq.supabase.co/storage/v1/object/public/images/static/logo_phucuong%20350px.png" />
+      <NextImage
+        src="https://nuwwaqzrwtilsxbajubq.supabase.co/storage/v1/object/public/images/static/logo_phucuong%20350px.png"
+        unoptimized
+        alt="package-icon"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+      />
     </a>
   );
 };
