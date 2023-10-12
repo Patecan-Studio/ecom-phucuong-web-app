54588b29540f2df27bcac583c1a20a10f8e10945
Phi Nguyen <quangphi1231@gmail.com>
2023-10-13 00:30:27 +0700

    finish updating for icons

--- src/components/common/Icons/TwentyFourHourIcon.tsx
+++ src/components/common/Icons/TwentyFourHourIcon.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import NextImage from "next/image";
 
 interface TwentyFourHourIconProps
   extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
 const TwentyFourHourIcon = (props: TwentyFourHourIconProps) => {
-  return <img src="./images/24h-icon.png" {...props}/>;
+  return (
+    <div {...props}>
+      <NextImage
+        src="/images/24h-icon.png"
+        alt="24h-icon"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+      />
+    </div>
+  );
 };
 
 export default TwentyFourHourIcon;
