0d21a68896d5d6eb484c275eb9dc022daa856eed
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2023-10-13 22:14:07 +0700

    add footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -4,6 +4,51 @@ import { TabbarLogo } from "..";
 import FacebookIcon from "../Icons/FacebookIcon";
 import YoutubeIcon from "../Icons/YoutubeIcon";
 import InstagramIcon from "../Icons/InstagramIcon";
+import { Collapse } from "antd";
+import type { CollapseProps } from "antd";
+const contact = (
+  <div className="contact">
+    <span>Hotline Cskh</span>
+    <span>02973.81.81.81</span>
+    <span>cs.noithatphucuong@gmail.com</span>
+  </div>
+);
+
+const support = (
+  <div className="support">
+    <span>Chế độ bảo hành</span>
+    <span>Câu hỏi thường gặp</span>
+    <span>Hướng dẫn đặt hàng</span>
+    <span>Chính sách đổi trả hàng</span>
+    <span>Cách thức vận chuyển</span>
+    <span>Dịch vụ tư vấn thiết kế</span>
+  </div>
+);
+
+const about = (
+  <div className="about">
+    <span>Giới thiệu</span>
+    <span>Hệ thống phân phối</span>
+  </div>
+);
+
+const infoItems: CollapseProps["items"] = [
+  {
+    key: "1",
+    label: "Phú Cường",
+    children: about,
+  },
+  {
+    key: "2",
+    label: "Liên Hệ",
+    children: contact,
+  },
+  {
+    key: "3",
+    label: "Hỗ Trợ Khách Hàng",
+    children: support,
+  },
+];
 const Footer = () => {
   return (
     <div className="footer">
@@ -33,6 +78,24 @@ const Footer = () => {
           <div className="slide">
             <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/hansgrohe-Euronics-Viet-Nam-2.png" />
           </div>
+          <div className="slide">
+            <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/image-1.png" />
+          </div>
+          <div className="slide">
+            <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/hansgrohe-Euronics-Viet-Nam-2.png" />
+          </div>
+          <div className="slide">
+            <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/caesar-logo-1.png" />
+          </div>
+          <div className="slide">
+            <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/AVALLON-LOGO2_TM_Gray-2.png" />
+          </div>
+          <div className="slide">
+            <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/92941291_1122254314795138_1554983477768617984_n-removebg-preview-4.png" />
+          </div>
+          <div className="slide">
+            <img src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/AVALLON-LOGO2_TM_Gray-2.png" />
+          </div>
         </div>
       </div>
       <div className="footer__title">
@@ -43,26 +106,25 @@ const Footer = () => {
         </span>
       </div>
       <div className="footer__connection">
-        {/* logo */}
         <TabbarLogo />
-        {/* input */}
         <div className="footer__connection__inputWrapper">
-          <span>Đăng ký nhận các chương trình hấp dẫn của Showroom Nội thất PHÚ CƯỜNG</span>
+          <span>
+            Đăng ký nhận các chương trình hấp dẫn của Showroom Nội thất PHÚ
+            CƯỜNG
+          </span>
           <div className="inputGroup">
-          <input
-            className="input"
-            type="text"
-            placeholder="Nhập địa chỉ email..."
-          />
-          <div className="inputButton">
-            <span>Đăng ký</span>
+            <input
+              className="input"
+              type="text"
+              placeholder="Nhập địa chỉ email..."
+            />
+            <div className="inputButton">
+              <span style={{ whiteSpace: "nowrap" }}>Đăng ký</span>
+            </div>
           </div>
         </div>
-        </div>
-        
-        {/* social */}
         <div className="footer__connection__social">
-          <span>Kết nối với chúng tôi qua mạng xã hội</span>
+          <span>Kết nối với chúng tôi </span>
           <div className="iconWrapper">
             <div className="icon">
               <FacebookIcon />
@@ -75,6 +137,37 @@ const Footer = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="footer__info">
+        <div className="about">
+          <h3>PHU CUONG</h3>
+          <span>Giới thiệu</span>
+          <span>Hệ thống phân phối</span>
+        </div>
+        <div className="contact">
+          <h3>Liên Hệ</h3>
+          <span>Hotline Cskh</span>
+          <span>02973.81.81.81</span>
+          <span>cs.noithatphucuong@gmail.com</span>
+        </div>
+        <div className="support">
+          <h3>Hỗ Trợ Khách Hàng</h3>
+          <span>Chế độ bảo hành</span>
+          <span>Câu hỏi thường gặp</span>
+          <span>Hướng dẫn đặt hàng</span>
+          <span>Chính sách đổi trả hàng</span>
+          <span>Cách thức vận chuyển</span>
+          <span>Dịch vụ tư vấn thiết kế</span>
+        </div>
+      </div>
+      <div className="footer__info--mobile">
+        <Collapse
+          items={infoItems}
+          ghost
+          expandIconPosition={"end"}
+          bordered={false}
+          defaultActiveKey={["1"]}
+        />
       </div>
     </div>
   );
