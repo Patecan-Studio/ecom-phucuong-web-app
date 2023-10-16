9d7714eba0a04bc0528b1582452755bdce3768d1
Phi Nguyen <quangphi1231@gmail.com>
2023-10-16 22:34:42 +0700

    init overview component

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,11 +5,25 @@ import "@/styles/fonts.scss";
 import "@/styles/reset.css";
 import "@/styles/responsives.scss";
 import { ProductDescription } from "@/components/common";
+import { Overview } from "@/components/composite";
 
-const Page = () => {
+const getProduct = async (id: string) => {
+  try {
+    const response = await fetch(
+      `${process.env.SITE_DOMAIN}/api/v1/products/${id}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const res = await getProduct(params.id);
   return (
     <div>
-      product detail page
+      <Overview />
     </div>
   );
 };
---
+++ src/components/composite/Overview/Overview.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './style.scss'
+
+const Overview = () => {
+  return <div>Overview</div>;
+};
+
+export default Overview;
