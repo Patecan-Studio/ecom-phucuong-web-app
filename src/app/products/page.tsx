bc9b5b7937498616e92e8020afa4148bfd396ca1
Phi Nguyen <quangphi1231@gmail.com>
2023-10-22 15:35:03 +0700

    add position relative to the products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "@/styles/common.scss";
+import "@/styles/colors.scss";
+import "@/styles/fonts.scss";
+import "@/styles/reset.css";
+import "@/styles/responsives.scss";
+import styles from "./page.module.css";
+import { Products } from "@/components/common";
 
 const Page = () => {
-  return <div>products list</div>;
+  return (
+    <div className={styles.products}>
+      <Products />
+    </div>
+  );
 };
 
 export default Page;
