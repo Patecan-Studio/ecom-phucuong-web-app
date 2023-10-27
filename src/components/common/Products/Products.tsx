726c3fc41ba2559d0d48ac97728c7ec6004b06e4
Thien Trong <tranthientrong.it@gmail.com>
2023-10-27 18:22:18 +0700

    Add Category IDs

--- src/components/common/Products/Products.tsx
+++ src/components/common/Products/Products.tsx
@@ -10,7 +10,7 @@ const getProducts = async (
 ) => {
     try {
         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/products?category=${category ? category : "all"}&page=${page}&page_size=${pageSize}&q=${q}`
+            `https://ecom-phucuong-inventory-develop.up.railway.app/api/v1/products?category=653b4c7085e86f091c56cb06&page=1&page_size=8&q=`
         );
         const data = await response.json();
         return data;
