dd6ffa9edd7493ec3dd0bbec44e96aa6e3aebf1d
Thien Trong <tranthientrong.it@gmail.com>
2023-10-27 17:43:33 +0700

    Add Category IDs

--- src/components/common/Tabbar/TabbarDropdown.tsx
+++ src/components/common/Tabbar/TabbarDropdown.tsx
@@ -5,8 +5,8 @@ const TabbarDropdown = ({ dropdownMenu }: TabbarDropdownProps) => {
   return (
     <div className="tabbar__dropdown">
       {dropdownMenu.map((item) => (
-        <div className="tabbar__dropdown__item" key={item.label.name}>
-          <div className="tabbar__dropdown__label hover:text-blue-500"><a href={`${item.label.path}`} key={item.label.name}>{item.label.name}</a></div>
+        <div className="tabbar__dropdown__item" key={(item.label as any).name}>
+          <div className="tabbar__dropdown__label hover:text-blue-500"><a href={`${(item.label as any).path!}`} key={(item.label as any).name}>{(item.label as any).name}</a></div>
           <div className="tabbar__dropdown__list">
             {item.list.map((subItem) => (
               <a href={subItem.path} key={subItem.name}>
