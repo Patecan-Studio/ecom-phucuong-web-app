9027cae2ceb42ff9b02428531465150b5ad55a0d
Thien Trong <tranthientrong.it@gmail.com>
2023-10-29 22:52:27 +0700

    [FEATURE] If no L x H x W --> no render

--- src/components/composite/Overview/OverviewAbout.tsx
+++ src/components/composite/Overview/OverviewAbout.tsx
@@ -67,31 +67,38 @@ const OverviewAbout = ({
           </div>
         </div>
       )}
-      <div className="overview__about__list">
-        <h5 className="overview__about__list__title">Chiều dài</h5>
-        <div className="overview__about__items">
-          <p className="overview__about__item no-border">{length}</p>
-        </div>
-      </div>
-      <div className="overview__about__list">
-        <h5 className="overview__about__list__title">Chiều rộng</h5>
-        <div className="overview__about__items">
-          <p className="overview__about__item no-border">{width}</p>
-        </div>
-      </div>
-      <div className="overview__about__list">
-        <h5 className="overview__about__list__title">Chiều cao</h5>
-        <div className="overview__about__items">
-          <p className="overview__about__item no-border">{height}</p>
-        </div>
-      </div>
-      <div className="overview__about__list">
-        <h5 className="overview__about__list__title">Cân nặng</h5>
-        <div className="overview__about__items">
-          <p className="overview__about__item no-border">{weight}</p>
-        </div>
-      </div>
-
+        {length.match(/\d+/) && length.match(/\d+/)![0] !== '0' && (
+            <div className="overview__about__list">
+                <h5 className="overview__about__list__title">Chiều dài</h5>
+                <div className="overview__about__items">
+                    <p className="overview__about__item no-border">{length}</p>
+                </div>
+            </div>
+        )}
+        {width.match(/\d+/) && width.match(/\d+/)![0] !== '0' && (
+            <div className="overview__about__list">
+                <h5 className="overview__about__list__title">Chiều rộng</h5>
+                <div className="overview__about__items">
+                    <p className="overview__about__item no-border">{width}</p>
+                </div>
+            </div>
+        )}
+        {height.match(/\d+/) && height.match(/\d+/)![0] !== '0' && (
+            <div className="overview__about__list">
+                <h5 className="overview__about__list__title">Chiều cao</h5>
+                <div className="overview__about__items">
+                    <p className="overview__about__item no-border">{height}</p>
+                </div>
+            </div>
+        )}
+        {weight.match(/\d+/) && length.match(/\d+/)![0] !== '0' && (
+            <div className="overview__about__list">
+                <h5 className="overview__about__list__title">Trọng lượng</h5>
+                <div className="overview__about__items">
+                    <p className="overview__about__item no-border">{weight}</p>
+                </div>
+            </div>
+        )}
       <div className="overview__about__reset" onClick={onResetVariant}>
         Reset
       </div>
