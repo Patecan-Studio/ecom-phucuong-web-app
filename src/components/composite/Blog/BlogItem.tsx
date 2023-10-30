cd3bba3a4565b8ae6c027a4273e40a6ab69ba7c9
Phi Nguyen <quangphi1231@gmail.com>
2023-10-30 22:11:57 +0700

    finish the blog for desktop

--- src/components/composite/Blog/BlogItem.tsx
+++ src/components/composite/Blog/BlogItem.tsx
@@ -1,18 +1,23 @@
+import ArrowRightIcon from "@/components/common/Icons/ArrowRightIcon";
 import React from "react";
 
 const BlogItem = ({ blog }: any) => {
-  return <div className="blog__item">
-    <div className="blog__item__image">
-      <img src={blog.image} alt="" />
+  return (
+    <div className="blog__item">
+      <div className="blog__item__image">
+        <img src={blog.image} alt="" />
+      </div>
+      <div className="blog__item__texts">
+        <h4 className="blog__item__type">{blog.type}</h4>
+        <h3 className="blog__item__title">
+          <a href={blog.link}>{blog.title}</a>
+        </h3>
+        <p className="blog__item__date">
+          {blog.date} <ArrowRightIcon width={50} height={50} />
+        </p>
+      </div>
     </div>
-    <div className="blog__item__texts">
-      <h4 className="blog__item__type">{blog.type}</h4>
-      <h3 className="blog__item__title">
-        <a href={blog.link}>{blog.title}</a>
-      </h3>
-      <p className="blog__item__date">{blog.date}</p>
-    </div>
-  </div>;
+  );
 };
 
 export default BlogItem;
