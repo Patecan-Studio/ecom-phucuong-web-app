9abe4e0a4ca5850686871c424738693d92b7bb4d
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2023-11-04 12:09:53 +0700

    fix layout

--- src/components/composite/Overview/OverviewAbout.tsx
+++ src/components/composite/Overview/OverviewAbout.tsx
@@ -83,33 +83,6 @@ const OverviewAbout = ({
           <h5 className="overview__about__list__title">Chiều cao</h5>
           <div className="overview__about__items">
             <p className="overview__about__item no-border">{height}</p>
-            {colors.map((item: any) => (
-              <button
-                className={`overview__about__item ${
-                  selectedColor === item.value &&
-                  dictionary[item.value]?.includes(selectedMaterial) &&
-                  "active"
-                }`}
-                key={item.value}
-                onClick={() => onColorSelect(item.value)}
-                disabled={
-                  dictionary[item.value] === undefined
-                    ? false
-                    : !dictionary[item.value].includes(selectedMaterial)
-                }
-              >
-                <div
-                  className="square-color"
-                  style={{
-                    background: item.value,
-                    width: "12px",
-                    height: "12px",
-                    borderRadius: "3px",
-                  }}
-                ></div>
-                {item.label}
-              </button>
-            ))}
           </div>
         </div>
       )}
