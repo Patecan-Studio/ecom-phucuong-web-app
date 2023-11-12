85fc885391a26e8f62ce40da3e918a6ea85cdc84
taitt <tantai1297@gmail.com>
2023-11-12 13:12:34 +0700

    fix css

--- src/components/UserComponent/UserSider/UserSider.tsx
+++ src/components/UserComponent/UserSider/UserSider.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./UserSider.scss"
 
-const UserSider = () => {
+type Props = {
+  setIntergrateScreen: any;
+}
+
+const UserSider = (props: Props) => {
   return (
     <div className='user-sider'>
       <div className="user-sider__wrapper">
         <ul className='user-sider__wrapper__ul'>
-          <div>Trang tài khoản</div>
-          <div>Đơn hàng</div>
-          <div>Địa chỉ</div>
-          <div>Tài khoản</div>
+          <div onClick={() => props.setIntergrateScreen('main')}>Trang tài khoản</div>
+          <div onClick={() => props.setIntergrateScreen('order')}>Đơn hàng</div>
+          <div onClick={() => props.setIntergrateScreen('address')}>Địa chỉ</div>
+          <div onClick={() => props.setIntergrateScreen('account')}>Tài khoản</div>
           <div>Thoát</div>
         </ul>
       </div>
