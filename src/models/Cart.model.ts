33df5ac02d769d0786097b833efa9e28ab09bcd4
hoanggia7398 <44608643+hoanggia7398@users.noreply.github.com>
2023-11-20 22:12:20 +0700

    fix build

--- src/models/Cart.model.ts
+++ src/models/Cart.model.ts
@@ -1,79 +1,80 @@
-import mongoose, {model, Schema} from "mongoose";
-import {ObjectId} from "mongodb";
-import {ProductVariantStatus} from "@/models/Product.model";
+import mongoose, { model, Schema } from "mongoose";
+import { ObjectId } from "mongodb";
+import { ProductVariantStatus } from "@/models/Product.model";
 
 const cartSchema = new mongoose.Schema(
-    {
-        productsList: [
-            {
-                product_id: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Product",
-                    required: true,
-                },
-                sku: { type: String },
-                product_brand_id: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Brand",
-                },
-                product_category_ids:[ {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Category",
-                }],
-                product_name: {type: String},
-                product_slug: {type: String},
-                product_variant_image: {type: String},
-                product_weight: {type: String},
-                product_height: {type: String},
-                product_width: {type: String},
-                product_length: {type: String},
-                product_size_unit: {type: String},
-                variant: {
-                    sku: String,
-                    color: {
-                        type: Schema.Types.Mixed,
-                        default: null,
-                        label: { type: String },
-                        value: { type: String },
-                    },
-                    material: String,
-                    price: Number,
-                    discount_price: Number,
-                    discount_percentage: Number,
-                    quantity: Number,
-                    image_list: [
-                        {
-                            imageName: String,
-                            imageUrl: String,
-                        },
-                    ],
-                    status: {
-                        type: String,
-                        default: ProductVariantStatus.Active,
-                        required: false,
-                    },
-                },
-                product_warranty: {type: String},
-                qty: {type: Number},
-                price: {type: Number},
-                discount_price: {type: Number},
-                discount_percentage: {type: Number},
-                totalAfterDiscount: {type: Number},
-            }
-        ],
-        cartTotal: Number,
-        totalAfterDiscount: Number,
-        user: {
+  {
+    productsList: [
+      {
+        product_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        sku: { type: String },
+        product_brand_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Brand",
+        },
+        product_category_ids: [
+          {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        }
+            ref: "Category",
+          },
+        ],
+        product_name: { type: String },
+        product_slug: { type: String },
+        product_variant_image: { type: String },
+        product_weight: { type: String },
+        product_height: { type: String },
+        product_width: { type: String },
+        product_length: { type: String },
+        product_size_unit: { type: String },
+        variant: {
+          sku: String,
+          color: {
+            type: Schema.Types.Mixed,
+            default: null,
+            label: { type: String },
+            value: { type: String },
+          },
+          material: String,
+          price: Number,
+          discount_price: Number,
+          discount_percentage: Number,
+          quantity: Number,
+          image_list: [
+            {
+              imageName: String,
+              imageUrl: String,
+            },
+          ],
+          status: {
+            type: String,
+            default: ProductVariantStatus.Active,
+            required: false,
+          },
+        },
+        product_warranty: { type: String },
+        qty: { type: Number },
+        price: { type: Number },
+        discount_price: { type: Number },
+        discount_percentage: { type: Number },
+        totalAfterDiscount: { type: Number },
+      },
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    {
-        collection: "carts",
-        timestamps: true,
-    }
+  },
+  {
+    collection: "carts",
+    timestamps: true,
+  }
 );
-
 
 const CartModel = mongoose.models.Cart || mongoose.model("Cart", cartSchema);
 
--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.scss";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import db from "@/utils/db";
 import UserModel from "@/models/User.model";
 import CartModel from "@/models/Cart.model";
@@ -8,106 +8,95 @@ import CheckoutPaymentMethod from "@/components/checkout/CheckoutPaymentMethod/C
 import CheckoutProductsCart from "@/components/checkout/CheckoutProductCart/CheckoutProductsCart";
 import CheckoutSummary from "@/components/checkout/CheckoutSummary/CheckoutSummary";
 
-
-
 interface CheckoutProps {
-    cart: any,
-    user: any
+  cart: any;
+  user: any;
 }
 
 interface Address {
-    firstName: string | '',
-    lastName: string | '',
-    phoneNumber: string | '',
-    email: string | '',
-    country: string | '',
-    city: string | '',
-    district:string | '',
-    ward: string | '',
-    address1: string | '',
-    zipCode: string | '',
-    active: boolean | false
+  firstName: string | "";
+  lastName: string | "";
+  phoneNumber: string | "";
+  email: string | "";
+  country: string | "";
+  city: string | "";
+  district: string | "";
+  ward: string | "";
+  address1: string | "";
+  zipCode: string | "";
+  active: boolean | false;
 }
 
-
-
 // eslint-disable-next-line @next/next/no-async-client-component
-export default function Checkout({cart, user}:CheckoutProps) {
+export default function Checkout({ cart, user }: CheckoutProps) {
+  const [addresses, setAddresses] = useState(user.address ?? []);
+  const [selectedAddress, setSelectedAddress] = useState(null);
+  const [paymentMethod, setPaymentMethod] = useState(null);
+  const [totalAfterDiscount, setTotalAfterDiscount] = useState(0);
 
-
-    const [addresses, setAddresses] = useState(user.address ?? []);
-    const [selectedAddress, setSelectedAddress] = useState(null);
-    const [paymentMethod, setPaymentMethod] = useState(null);
-    const [totalAfterDiscount, setTotalAfterDiscount] = useState(0);
-
-    useEffect(() => {
-        let whichAddressIsActive = addresses.find((address: Address) => address.active == true);
-        if (whichAddressIsActive) {
-            setSelectedAddress(whichAddressIsActive);
-        } else {
-            setSelectedAddress(addresses[0] ?? "");
-        }
-    }, [addresses]);
-
-
-    return (
-        <div className={`${styles.container} ${styles.checkout}`}>
-            <div className={styles.checkout_side}>
-                <Shipping
-                    selectedAddress={selectedAddress}
-                    setSelectedAddress={setSelectedAddress}
-                    user={user}
-                    addresses={addresses}
-                    setAddresses={setAddresses}
-                />
-                <CheckoutProductsCart
-                    cart={cart}
-                />
-            </div>
-            <div className={styles.checkout_side}>
-                <CheckoutPaymentMethod
-                    paymentMethod={paymentMethod}
-                    setPaymentMethod={setPaymentMethod}
-                />
-                <CheckoutSummary
-                    totalAfterDiscount={totalAfterDiscount}
-                    setTotalAfterDiscount={setTotalAfterDiscount}
-                    paymentMethod={paymentMethod!}
-                    selectedAddress={selectedAddress}
-                    cart={cart}
-                    user={user}
-                />
-            </div>
-            <div className={styles.checkout_side}></div>
-
-        </div>
-
+  useEffect(() => {
+    let whichAddressIsActive = addresses.find(
+      (address: Address) => address.active == true
     );
+    if (whichAddressIsActive) {
+      setSelectedAddress(whichAddressIsActive);
+    } else {
+      setSelectedAddress(addresses[0] ?? "");
+    }
+  }, [addresses]);
+
+  return (
+    <div className={`${styles.container} ${styles.checkout}`}>
+      <div className={styles.checkout_side}>
+        <Shipping
+          selectedAddress={selectedAddress}
+          setSelectedAddress={setSelectedAddress}
+          user={user}
+          addresses={addresses}
+          setAddresses={setAddresses}
+        />
+        <CheckoutProductsCart cart={cart} />
+      </div>
+      <div className={styles.checkout_side}>
+        <CheckoutPaymentMethod
+          paymentMethod={paymentMethod}
+          setPaymentMethod={setPaymentMethod}
+        />
+        <CheckoutSummary
+          totalAfterDiscount={totalAfterDiscount}
+          setTotalAfterDiscount={setTotalAfterDiscount}
+          paymentMethod={paymentMethod!}
+          selectedAddress={selectedAddress}
+          cart={cart}
+          user={user}
+        />
+      </div>
+      <div className={styles.checkout_side}></div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context: any) {
-    console.log("GET DATA");
-    await db.connectDb();
-    const user = await UserModel.findById('655609014186e2628008b45d');
-    console.log(user);
-    const cart = await CartModel.findOne({user: user._id});
-    console.log(user);
+  console.log("GET DATA");
+  await db.connectDb();
+  const user = await UserModel.findById("655609014186e2628008b45d");
+  console.log(user);
+  const cart = await CartModel.findOne({ user: user._id });
+  console.log(user);
 
-    db.disconnectDb();
-    if (!cart) {
-        return {
-            redirect: {
-                destination: "/cart",
-            }
-        }
-    }
-
+  db.disconnectDb();
+  if (!cart) {
     return {
-        props: {
-            cart: JSON.parse(JSON.stringify(cart)),
-            user: JSON.parse(JSON.stringify(user))
-        }
-    }
+      redirect: {
+        destination: "/cart",
+      },
+    };
+  }
 
+  return {
+    props: {
+      cart: JSON.parse(JSON.stringify(cart)),
+      user: JSON.parse(JSON.stringify(user)),
+    },
+  };
 }
-
