90d1b7e3c08d4653f1513688450a905dd1edec65
Phi Nguyen <quangphi1231@gmail.com>
2023-12-08 00:56:34 +0700

    add hover and zoom function

--- src/components/composite/Overview/OverviewMainImages.tsx
+++ src/components/composite/Overview/OverviewMainImages.tsx
@@ -17,6 +17,20 @@ const OverviewMainImagesInner = (
               fill
               onClick={onOpenModal}
               preload="false"
+              onMouseMove={(e: any) => {
+                const x = e.nativeEvent.offsetX;
+                const y = e.nativeEvent.offsetY;
+                const width = e.target.width;
+                const height = e.target.height;
+                const xPercent = (x / width) * 100;
+                const yPercent = (y / height) * 100;
+                e.target.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+                e.target.style.transform = "scale(2)";
+              }}
+              onMouseLeave={(e: any) => {
+                e.target.style.transformOrigin = "center center";
+                e.target.style.transform = "scale(1)";
+              }}
             />
           </div>
         ))}
