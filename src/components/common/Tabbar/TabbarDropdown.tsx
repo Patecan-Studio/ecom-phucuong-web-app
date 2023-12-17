ac9ab1673b39d57a7ee200c7566a772d104c707b
Phi Nguyen <quangphi1231@gmail.com>
2023-12-17 15:28:57 +0700

    add toggle button to menu

--- src/components/common/Tabbar/TabbarDropdown.tsx
+++ src/components/common/Tabbar/TabbarDropdown.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import { TabbarDropdownProps } from "./types";
 import Link from "next/link";
--- src/components/common/Tabbar/TabbarSecondaryMobile.tsx
+++ src/components/common/Tabbar/TabbarSecondaryMobile.tsx
@@ -1,33 +1,53 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import TabbarDropdown from "./TabbarDropdown";
 import Link from "next/link";
 
 const TabbarSecondaryMobile = ({ menu, checkExternalLink }: any) => {
+  const [mobileMenu, setMobileMenu] = useState(
+    menu.map((item: any) => {
+      if (item.dropdownMenu) {
+        return { ...item, isOpen: false };
+      }
+      return item;
+    })
+  );
+
+  const toggleDropdown = (index: number) => {
+    const newMobileMenu = [...mobileMenu];
+    newMobileMenu[index].isOpen = !newMobileMenu[index].isOpen;
+    setMobileMenu(newMobileMenu);
+  };
+
   return (
     <div className="tabbar--secondaryMobile">
-      {menu.map((item: any) => (
-        <div key={item.label}>
-          <div className="tabbar--secondaryMobile__item">
-            {checkExternalLink(item.path) ? (
-              <a
-                className="tabbar--secondaryMobile__link"
-                href={item.path}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {item.label}
-              </a>
-            ) : (
-              <Link
-                className="tabbar--secondaryMobile__link"
-                href={item.path}
-              >
-                {item.label}
-              </Link>
-            )}
-          </div>
-          {item.dropdownMenu && (
+      {mobileMenu.map((item: any, index: number) => (
+        <div
+          key={item.label}
+          className="tabbar--secondaryMobile__item"
+          onClick={() => toggleDropdown(index)}
+        >
+          {checkExternalLink(item.path) ? (
+            <a
+              className="tabbar--secondaryMobile__link"
+              href={item.path}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.label}
+            </a>
+          ) : (
+            <Link
+              className={`tabbar--secondaryMobile__link ${
+                item.isOpen ? "active" : ""
+              }`}
+              href={item.path}
+            >
+              {item.label}
+            </Link>
+          )}
+
+          {item.dropdownMenu && item?.isOpen && (
             <TabbarDropdown dropdownMenu={item.dropdownMenu} />
           )}
         </div>
