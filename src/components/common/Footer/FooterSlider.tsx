aa4cb35c9cba91542abfdb87f3c6315cf6e2ab0d
gia.nguyen <gia.nguyen@s3corp.com.vn>
2023-12-20 12:45:32 +0700

    add logo

---
+++ src/components/common/Footer/FooterSlider.tsx
@@ -0,0 +1,142 @@
+"use client"
+import React from "react";
+import { CustomImage } from "..";
+import "./style.scss";
+
+const FooterSlider = () => {
+  return (
+    <div className="footer__slider">
+      <div className="slide-track">
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 1"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/LOGO%20CRAFURNI%2001032023.png?t=2023-12-20T05%3A28%3A26.040Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 2"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/Eurotile-01.png?t=2023-12-20T05%3A30%3A59.473Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 3"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/logo%20-lakinh-2022-02.png?t=2023-12-20T05%3A34%3A22.838Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 4"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/1.png?t=2023-12-20T05%3A35%3A37.858Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 5"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/genus-elite.png?t=2023-12-20T05%3A36%3A02.435Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 6"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/NEW%20INAX-04.png?t=2023-12-20T05%3A36%3A27.140Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 7"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/interwood.png"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 8"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/hansgrohe-Euronics-Viet-Nam-2.png"
+          />
+        </div>
+        {/* loop here */}
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 9"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/image-1.png"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 10"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/hansgrohe-Euronics-Viet-Nam-2.png"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 11"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/caesar-logo-1.png"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 12"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/AVALLON-LOGO2_TM_Gray-2.png"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 13"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/92941291_1122254314795138_1554983477768617984_n-removebg-preview-4.png"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 14"
+            src="https://demo4d.thuythu.com/vatlieuxaydung/wp-content/uploads/2022/05/AVALLON-LOGO2_TM_Gray-2.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterSlider;
