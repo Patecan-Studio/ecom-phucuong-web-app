1b0c60c76a2d78a8afc0825f2504ed812711c4c3
ext-phi.nguyen <ext-phi.nguyen@spdigital.sg>
2023-12-20 16:34:39 +0700

    fix banner section

--- src/components/common/Carousel/CarouselSlideMain.tsx
+++ src/components/common/Carousel/CarouselSlideMain.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from "react";
 import { CarouselSlide } from "./types";
+import Link from "next/link";
 
 const CarouselSlideMain = ({ slide }: CarouselSlide) => {
   return (
-    <div className="carousel__slide">
+    <Link href={slide?.link || ""} className="carousel__slide">
       <div className="carousel__image">{slide?.image as ReactNode}</div>
-    </div>
+    </Link>
   );
 };
 
