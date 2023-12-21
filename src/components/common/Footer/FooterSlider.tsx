44b68d28ab37d6138af621fc3abd1913eb0b7bdf
gia.nguyen <gia.nguyen@s3corp.com.vn>
2023-12-21 09:36:18 +0700

    update logo footer slider

--- src/components/common/Footer/FooterSlider.tsx
+++ src/components/common/Footer/FooterSlider.tsx
@@ -13,98 +13,99 @@ const FooterSlider = () => {
             height={0}
             sizes="100vw"
             alt="footer slide 1"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/Home%20PA-21.png?t=2023-12-20T14%3A02%3A46.696Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/crafurni.png?t=2023-12-21T02%3A24%3A06.950Z"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 2"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/crafuni-21.png?t=2023-12-20T14%3A02%3A56.627Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/dspace.png?t=2023-12-21T02%3A24%3A21.815Z"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 3"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/eurotile-21.png?t=2023-12-20T14%3A03%3A10.196Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/eurotile.png"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 4"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/galaxy-21.png?t=2023-12-20T14%3A03%3A20.685Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/galaxy.png"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 5"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/inax.png?t=2023-12-20T14%3A03%3A31.635Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/inax.png?t=2023-12-21T02%3A24%3A51.787Z"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 6"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/kes-21.png?t=2023-12-20T14%3A11%3A50.220Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/interwood.png?t=2023-12-21T02%3A25%3A02.305Z"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 7"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/interwood-21.png?t=2023-12-20T14%3A04%3A04.626Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/kes.png?t=2023-12-21T02%3A26%3A44.577Z"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 8"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/malloca-21.png?t=2023-12-20T14%3A04%3A22.916Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/malloca.png?t=2023-12-21T02%3A26%3A56.237Z"
           />
         </div>
+
         {/* loop here */}
-        <div className="slide">
-          <CustomImage
-            width={0}
-            height={0}
-            sizes="100vw"
-            alt="footer slide 9"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/crafuni-21.png?t=2023-12-20T14%3A02%3A56.627Z"
-          />
-        </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 10"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/royal-21.png?t=2023-12-20T14%3A04%3A44.380Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/nem-kim-cuong.png?t=2023-12-21T02%3A27%3A21.264Z"
           />
         </div>
+
         <div className="slide">
           <CustomImage
             width={0}
             height={0}
             sizes="100vw"
             alt="footer slide 11"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/viglacera-21.png?t=2023-12-20T14%3A05%3A06.456Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/phucuonggroup.png?t=2023-12-21T02%3A27%3A32.523Z"
           />
         </div>
         <div className="slide">
@@ -113,7 +114,7 @@ const FooterSlider = () => {
             height={0}
             sizes="100vw"
             alt="footer slide 12"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/inax.png?t=2023-12-20T14%3A03%3A31.635Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/phucuonghome.png?t=2023-12-21T02%3A27%3A42.975Z"
           />
         </div>
         <div className="slide">
@@ -122,7 +123,7 @@ const FooterSlider = () => {
             height={0}
             sizes="100vw"
             alt="footer slide 13"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/interwood-21.png?t=2023-12-20T14%3A04%3A04.626Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/royal.png?t=2023-12-21T02%3A27%3A52.592Z"
           />
         </div>
         <div className="slide">
@@ -131,7 +132,35 @@ const FooterSlider = () => {
             height={0}
             sizes="100vw"
             alt="footer slide 14"
-            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider/malloca-21.png?t=2023-12-20T14%3A04%3A22.916Z"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/vilacera.png?t=2023-12-21T02%3A28%3A41.885Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 14"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/vuong.png?t=2023-12-21T02%3A28%3A54.475Z"
+          />
+        </div>
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 1"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/crafurni.png?t=2023-12-21T02%3A24%3A06.950Z"
+          />
+        </div>
+
+        <div className="slide">
+          <CustomImage
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="footer slide 2"
+            src="https://djwgwcdcgfsknzddrchp.supabase.co/storage/v1/object/public/images/footer-slider2/dspace.png?t=2023-12-21T02%3A24%3A21.815Z"
           />
         </div>
       </div>
