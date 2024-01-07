696206bfbc0450a33b77e845119e758c6dff27e6
Phi Nguyen <quangphi1231@gmail.com>
2024-01-07 23:29:26 +0700

    add defer the facebook script

--- src/components/common/Facebook/Chatbox.tsx
+++ src/components/common/Facebook/Chatbox.tsx
@@ -21,6 +21,7 @@ const ChatBot = () => {
       js = d.createElement(s);
       js.id = id;
       js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+      js.defer = true;
       fjs.parentNode.insertBefore(js, fjs);
     })(document, "script", "facebook-jssdk");
   }, []);
