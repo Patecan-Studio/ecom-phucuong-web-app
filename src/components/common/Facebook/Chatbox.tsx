99dcf4f714c5f890fc104a9c0d90d7e5d54bcde8
Phi Nguyen <quangphi1231@gmail.com>
2024-01-08 02:06:52 +0700

    re use the old script

--- src/components/common/Facebook/Chatbox.tsx
+++ src/components/common/Facebook/Chatbox.tsx
@@ -1,35 +1,40 @@
 // @ts-nocheck
 "use client";
-import Script from "next/script";
+import { useEffect, useRef } from "react";
 
 const ChatBot = () => {
-  if (typeof window === "undefined") return null;
+  const MessengerRef = useRef();
+  useEffect(() => {
+    MessengerRef.current.setAttribute("page_id", "105032941775735");
+    MessengerRef.current.setAttribute("attribution", "biz_inbox");
 
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v18.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.defer = true;
+      js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
   return (
-    <Script
-      strategy="lazyOnload"
-      src="https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js"
-      defer
-      onLoad={() => {
-        window.fbAsyncInit = function () {
-          window.FB.init({
-            xfbml: true,
-            version: "v18.0",
-          });
-        };
-
-        (function (d, s, id) {
-          var js,
-            fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s);
-          js.id = id;
-          js.src =
-            "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        })(document, "script", "facebook-jssdk");
-      }}
-    />
+    <>
+      <div id="fb-root"></div>
+      <div
+        ref={MessengerRef}
+        id="fb-customer-chat"
+        className="fb-customerchat"
+      ></div>
+    </>
   );
 };
 export default ChatBot;
